ccdd46eeeb10d3be2e19fdfe137b255032c54cde
Rabindra Joshi <therj@users.noreply.github.com>
2018-06-26 03:34:14 +0545

    move react-script from dev to dependencies

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class App extends Component {
     if (result) {
       result = math.eval(result)
       result = math.format(result, { precision: 14 })
-      result = String(result)
       this.setState({
         operations: [result],
       })
