00bcd4fdbefd01051188867970c217abf0dbbf28
Rabindra Joshi <therj@users.noreply.github.com>
2018-06-25 20:38:53 +0545

    component breakdown

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import update from 'immutability-helper'
+import math from 'mathjs'
+import './App.css'
+import Display from './Display'
+import Button from './Button'
+import Buttons from './Buttons'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { operations: [] }
+  }
+
+  calculateOperations = () => {
+    let result = this.state.operations.join('')
+    if (result) {
+      result = math.eval(result)
+      result = math.format(result, { precision: 14 })
+      result = String(result)
+      this.setState({
+        operations: [result],
+      })
+    }
+  }
+  handleClick = e => {
+    const value = e.target.getAttribute('data-value')
+    switch (value) {
+      case 'clear':
+        this.setState({
+          operations: [],
+        })
+        break
+      case 'equal':
+        this.calculateOperations()
+        break
+      default:
+        const newOperations = update(this.state.operations, {
+          $push: [value],
+        })
+        this.setState({
+          operations: newOperations,
+        })
+        break
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Display data={this.state.operations} />
+        <Buttons>
+          <Button onClick={this.handleClick} label="C" value="clear" />
+          <Button onClick={this.handleClick} label="7" value="7" />
+          <Button onClick={this.handleClick} label="4" value="4" />
+          <Button onClick={this.handleClick} label="1" value="1" />
+          <Button onClick={this.handleClick} label="0" value="0" />
+
+          <Button onClick={this.handleClick} label="/" value="/" />
+          <Button onClick={this.handleClick} label="8" value="8" />
+          <Button onClick={this.handleClick} label="5" value="5" />
+          <Button onClick={this.handleClick} label="2" value="2" />
+          <Button onClick={this.handleClick} label="." value="." />
+
+          <Button onClick={this.handleClick} label="x" value="*" />
+          <Button onClick={this.handleClick} label="9" value="9" />
+          <Button onClick={this.handleClick} label="6" value="6" />
+          <Button onClick={this.handleClick} label="3" value="3" />
+          <Button label="" value="null" />
+
+          <Button onClick={this.handleClick} label="-" value="-" />
+          <Button onClick={this.handleClick} label="+" size="2" value="+" />
+          <Button onClick={this.handleClick} label="=" size="2" value="equal" />
+        </Buttons>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Button.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+class Button extends Component {
+  render() {
+    return (
+      <div
+        onClick={this.props.onClick}
+        className="Button"
+        data-size={this.props.size}
+        data-value={this.props.value}
+      >
+        {this.props.label}
+      </div>
+    )
+  }
+}
+
+export default Button
